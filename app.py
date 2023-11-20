b325aa5fe824386458f0e766f4490337f30143a1
daffairfan45 <148300668+daffairfan45@users.noreply.github.com>
2023-11-20 15:40:57 +0700

    app.py

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
-import mysql.connector
+import pyodbc
 from flask import Flask, render_template, request, redirect, session, flash, url_for
 from functools import wraps
 
 app = Flask(__name__)
 
-config = {
-    'host': 'kelompok2databases.mysql.database.azure.com',
-    'user': 'daffa',
-    'password': 'Nusantara45_',
-    'database': 'db_login',  # Replace with your actual database name
-    'client_flags': [mysql.connector.ClientFlag.SSL],
-    'ssl_ca': 'DigiCertGlobalRootCA.crt.pem'
-}
+# Konfigurasi Azure SQL Server
+server = 'mysqllogin.database.windows.net'
+database = 'dbdbkelompok2'
+username = 'daffa'
+password = 'Nusantara45_'
+driver = '{ODBC Driver 18 for SQL Server}'
 
-# Establish a MySQL connection
-mysql_conn = mysql.connector.connect(**config)
+# Membangun koneksi ke Azure SQL Server
+conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+sql_conn = pyodbc.connect(conn_str)
 
-# Login
+# Fungsi decorator untuk memeriksa apakah pengguna sudah login
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Mohon login terlebih dahulu', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+# Halaman login
 @app.route('/')
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -24,17 +34,17 @@ def login():
     if request.method == 'POST':
         email = request.form["email"]
         pwd = request.form["pass"]
-        cur = mysql_conn.cursor(dictionary=True)
-        cur.execute("SELECT * FROM login WHERE EMAIL=%s AND PASS=%s", (email, pwd))
-        data = cur.fetchone()
-        cur.close()
+        cursor = sql_conn.cursor()
+        cursor.execute("SELECT * FROM dblogin WHERE EMAIL=? AND PASS=?", (email, pwd))
+        data = cursor.fetchone()
+        cursor.close()
         if data:
             session['logged_in'] = True
-            session['username'] = data["NAMA"]
-            flash('Login Successfully', 'success')
+            session['username'] = data.NAMA
+            flash('Login Berhasil', 'success')
             return redirect('home')
         else:
-            flash('Invalid Login. Try Again', 'danger')
+            flash('Login Gagal. Coba lagi', 'danger')
     return render_template("login.html")
 
 # check if the user is logged in
@@ -56,9 +66,9 @@ def reg():
         nama = request.form["nama"]
         email = request.form["email"]
         pwd = request.form["pass"]
-        cur = mysql_conn.cursor()
-        cur.execute("INSERT INTO login(NAMA, EMAIL, PASS) VALUES (%s, %s, %s)", (nama, email, pwd))
-        mysql_conn.commit()
+        cur = sql_conn.cursor()  # Ganti mysql_conn menjadi sql_conn
+        cur.execute("INSERT INTO dblogin(NAMA, EMAIL, PASS) VALUES (?, ?, ?)", (nama, email, pwd))  # Menggunakan tanda tanya (?) untuk parameter
+        sql_conn.commit()
         cur.close()
         flash('Registration Successfully. Login Here...', 'success')
         return redirect('login')
